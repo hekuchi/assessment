c7555f8f6bb43da44f5a25635de0594571a496d4
hekuchi <127936914+hekuchi@users.noreply.github.com>
2023-03-15 18:58:32 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -42,7 +42,7 @@ assessmentButton.onclick = () => {
     anchor.setAttribute('href', hrefValue);
     anchor.className = 'twitter-hashtag-button';
     anchor.setAttribute('data-text', result);
-    anchor.innerText = 'Tweet #あなたのいいところ https://hekuchi.github.io/assessment/assessment.html';
+    anchor.innerText = 'Tweet #あなたのいいところ';
     tweetDivided.appendChild(anchor);
 
     // widgets.js の設定
